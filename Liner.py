3df2e1495d7b84edaabfabd154fa5dc824cc64ed
Resist <93611543+Resisst@users.noreply.github.com>
2022-04-02 17:59:57 +0100

    Update Liner.py

--- Liner.py
+++ Liner.py
@@ -1,6 +1,4 @@
-from colorama import Fore
-
-file_name = input(f"{Fore.GREEN}[{Fore.CYAN}>>>{Fore.GREEN}]{Fore.RESET} Enter the file name: ");file_name = "".join([file_name + ".py" if not file_name.endswith(".py") else file_name])
+from colorama import Fore;file_name = input(f"{Fore.GREEN}[{Fore.CYAN}>>>{Fore.GREEN}]{Fore.RESET} Enter the file name: ");file_name = "".join([file_name + ".py" if not file_name.endswith(".py") else file_name])
 with open(file_name, "r") as f:
     lines = f.read();code = str(lines.encode("utf8"))[:-1][2:]
     with open(file_name[:-3]+"_oneline.py", "w") as f:
