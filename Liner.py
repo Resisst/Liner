a1be1ea195331298aead44c85ab32960280ab39d
Resist <93611543+Resisst@users.noreply.github.com>
2022-04-02 17:40:36 +0100

    Add files via upload

--- Liner.py
+++ Liner.py
@@ -4,4 +4,4 @@ file_name = input(f"{Fore.GREEN}[{Fore.CYAN}>>>{Fore.GREEN}]{Fore.RESET} Enter t
 with open(file_name, "r") as f:
     lines = f.read();code = str(lines.encode("utf8"))[:-1][2:]
     with open(file_name[:-3]+"_oneline.py", "w") as f:
-        f.write(f"exec(\"\"\"{code}\"\"\")")
+        f.write(f"exec(\"\"\"{code}\"\"\")");print(f"{Fore.GREEN}[{Fore.CYAN}>>>{Fore.GREEN}]{Fore.RESET} File saved as {file_name[:-3]+'_oneline.py'}")
---
+++ Liner_no_colorama.py
@@ -0,0 +1,7 @@
+from colorama import Fore
+
+file_name = input(f"[>>>] Enter the file name: ");file_name = "".join([file_name + ".py" if not file_name.endswith(".py") else file_name])
+with open(file_name, "r") as f:
+    lines = f.read();code = str(lines.encode("utf8"))[:-1][2:]
+    with open(file_name[:-3]+"_oneline.py", "w") as f:
+        f.write(f"exec(\"\"\"{code}\"\"\")");print(f"[>>>] File saved as {file_name[:-3]+'_oneline.py'}")
